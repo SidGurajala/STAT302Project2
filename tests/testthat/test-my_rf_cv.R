33abcef4a97395a20fa631ab975357eebc0fe2e8
SidGurajala <sgurajala99@gmail.com>
2020-12-12 16:36:59 -0800

    add more tests

--- tests/testthat/test-my_rf_cv.R
+++ tests/testthat/test-my_rf_cv.R
@@ -1,8 +1,11 @@
 test_that("output is right type", {
   expect_is(my_rf_cv(5), "numeric")
+  expect_is(my_rf_cv(10), "numeric")
+  expect_is(my_rf_cv(100), "numeric")
 })
 
 test_that("wrong input will throw errors", {
   expect_error(my_rf_cv(list(c("penguins"))))
   expect_error(my_rf_cv(matrix(NA, nrow = 1)))
+  expect_error(my_rf_cv(data.frame(NA)))
 })
