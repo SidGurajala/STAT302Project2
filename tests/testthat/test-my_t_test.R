371c04f6af9cca3a480b68ab0481bcd6e3c99157
SidGurajala <sgurajala99@gmail.com>
2020-12-12 16:42:07 -0800

    add more tests to test-my_t_test.R

--- tests/testthat/test-my_t_test.R
+++ tests/testthat/test-my_t_test.R
@@ -19,3 +19,25 @@ test_that("wrong kind of inputs throw errors", {
   expect_error(my_t_test(sample_data, 5, 12))
   expect_error(my_t_test(sample_data, "two.sided", "character"))
 })
+
+sample_output_2 <- my_t_test(sample_data, "less", 10)
+
+test_that("my_t_test_2 output is correct", {
+  expect_type(sample_output_2[[1]], "double")
+  expect_type(sample_output_2[[2]], "double")
+  expect_type(sample_output_2[[3]], "character")
+  expect_type(sample_output_2[[4]], "double")
+  expect_equal(sample_output_2[[3]], "less")
+  expect_equal(sample_output_2[[2]], 999)
+})
+
+sample_output_3 <- my_t_test(sample_data, "greater", 10)
+
+test_that("my_t_test_3 output is correct", {
+  expect_type(sample_output_3[[1]], "double")
+  expect_type(sample_output_3[[2]], "double")
+  expect_type(sample_output_3[[3]], "character")
+  expect_type(sample_output_3[[4]], "double")
+  expect_equal(sample_output_3[[3]], "greater")
+  expect_equal(sample_output_3[[2]], 999)
+})
